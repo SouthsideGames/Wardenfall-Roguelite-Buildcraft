712767ab22b72f3a581aafd939f785e4cdc886e1
SouthsideGames <southsidegames2021@gmail.com>
2024-09-08 01:54:26 -0400

    DaBoink Complete

--- Assets/Scripts/Weapons/Weapon.cs
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -121,6 +121,8 @@ public class Weapon : MonoBehaviour
         state = State.Attack;   
 
         damagedEnemies.Clear(); 
+
+        anim.speed = 1f / attackDelay;
     }
 
     private void EndAttack()
