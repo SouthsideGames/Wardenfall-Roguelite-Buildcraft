47a9e30d085028861d79c251b6cf62eb3f366953
Southside Games <94315165+SouthsideGames@users.noreply.github.com>
2025-04-26 18:55:31 -0400

    Create IceHazard.cs

---
+++ Assets/Scripts/Misc/IceHazard.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+/// <summary>
+/// Creates a slippery ice zone that reduces entity movement control
+/// </summary>
+public class IceHazard : EnvironmentalHazard
+{
+    [SerializeField] private float slowdownFactor = 0.5f;
+    [SerializeField] private Color iceColor = new Color(0.8f, 0.8f, 1f, 0.3f);
+    
+    private SpriteRenderer spriteRenderer;
+
+    protected override void Start()
+    {
+        base.Start();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (spriteRenderer != null)
+        {
+            spriteRenderer.color = iceColor;
+        }
+    }
+
+    protected override void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            CharacterController controller = other.GetComponent<CharacterController>();
+            if (controller != null)
+            {
+                controller.SetMovementMultiplier(slowdownFactor);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            CharacterController controller = other.GetComponent<CharacterController>();
+            if (controller != null)
+            {
+                controller.SetMovementMultiplier(1f);
+            }
+        }
+    }
+}
