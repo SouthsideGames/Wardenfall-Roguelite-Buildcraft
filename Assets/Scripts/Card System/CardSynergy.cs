4a98f433bcb45cb4d04ff733555bfc8b900f4db4
Southside Games <94315165+SouthsideGames@users.noreply.github.com>
2025-05-02 08:19:54 -0400

    Create CardSynergy.cs

---
+++ Assets/Scripts/Card System/CardSynergy.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[System.Serializable]
+public class CardSynergy
+{
+    public CardEffectType cardTypeA;
+    public CardEffectType cardTypeB;
+    public CardSO resultCard;
+}
