6aed6f37ca4c8130c5102132d89c9c42f2f3f08d
Southside Games <94315165+SouthsideGames@users.noreply.github.com>
2025-05-02 08:24:17 -0400

    Update SynergyManager.cs

--- Assets/Scripts/Card System/SynergyManager.cs
+++ Assets/Scripts/Card System/SynergyManager.cs
@@ -4,41 +4,21 @@ using UnityEngine;
 
 public class SynergyManager : MonoBehaviour
 {
-    [SerializeField] private List<CardSynergy> availableSynergies = new();
-    
-    public void CheckAndApplySynergies(CharacterCards characterCards)
+  public List<Synergy> synergies = new List<Synergy>();
+
+    public void CheckAndApplySynergies(CharacterCards deck)
     {
-        var deck = characterCards.Deck;
-        
-        foreach (var synergy in availableSynergies)
+        foreach (var synergy in synergies)
         {
-            CardSO cardA = null;
-            CardSO cardB = null;
-            
-            // Find matching cards in deck
-            foreach (var card in deck)
+            bool hasAllCards = synergy.requiredCards.All(cardID => deck.HasCard(cardID));
+            if (hasAllCards)
             {
-                if (card.effectType == synergy.cardTypeA && cardA == null)
-                    cardA = card;
-                else if (card.effectType == synergy.cardTypeB && cardB == null)
-                    cardB = card;
-                
-                if (cardA != null && cardB != null)
-                    break;
-            }
-            
-            // If we found both cards, apply synergy
-            if (cardA != null && cardB != null)
-            {
-                int indexA = deck.IndexOf(cardA);
-                int indexB = deck.IndexOf(cardB);
-                
-                // Remove both cards and add synergy card
-                characterCards.RemoveCard(indexA);
-                characterCards.RemoveCard(indexB > indexA ? indexB - 1 : indexB);
-                characterCards.AddCard(synergy.resultCard);
-                
-                Debug.Log($"Applied synergy: {cardA.cardName} + {cardB.cardName} = {synergy.resultCard.cardName}");
+                foreach (var requiredCard in synergy.requiredCards)
+                {
+                    deck.RemoveCard(deck.Deck.First(x=>x.cardID == requiredCard));
+                }
+                deck.AddCard(synergy.resultingCard);
+                break; // Apply only the first matching synergy
             }
         }
     }
