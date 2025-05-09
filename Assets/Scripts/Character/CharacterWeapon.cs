348c3e6b9db7b2b24b387cd1c2ec59702aa6c489
Southside Games <94315165+SouthsideGames@users.noreply.github.com>
2025-05-09 00:18:18 -0400

    Update CharacterWeapon.cs

--- Assets/Scripts/Character/CharacterWeapon.cs
+++ Assets/Scripts/Character/CharacterWeapon.cs
@@ -1,11 +1,24 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterWeapon : MonoBehaviour
 {
+    public static CharacterWeapon Instance { get; private set; }
+
     [Header("ELEMENTS:")]
     [SerializeField] private WeaponPosition[] weaponPositions;
+
+    private Dictionary<string, int> weaponUsageCounts = new Dictionary<string, int>();
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
 
     public bool AddWeapon(WeaponDataSO _selectedWeapon, int _weaponLevel)
     {
@@ -15,11 +28,35 @@ public class CharacterWeapon : MonoBehaviour
                continue;
 
             weaponPositions[i].AssignWeapon(_selectedWeapon.Prefab, _weaponLevel);
+            TrackWeaponUsage(_selectedWeapon.ID);
             return true;    
         }
 
-
         return false;
+    }
+
+    private void TrackWeaponUsage(string weaponId)
+    {
+        if (!weaponUsageCounts.ContainsKey(weaponId))
+            weaponUsageCounts[weaponId] = 0;
+        weaponUsageCounts[weaponId]++;
+    }
+
+    public string GetMostEffectiveWeapon()
+    {
+        string mostUsedWeaponId = "";
+        int maxUsage = 0;
+
+        foreach (var kvp in weaponUsageCounts)
+        {
+            if (kvp.Value > maxUsage)
+            {
+                maxUsage = kvp.Value;
+                mostUsedWeaponId = kvp.Key;
+            }
+        }
+
+        return mostUsedWeaponId;
     }
 
     public Weapon[] GetWeapons()
